d5f4d000cf8caf7536f7123d2b68fd468d5583b1
Illidane <Illidane@gmail.com>
2011-10-14 19:32:44 +0300

    now meat

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ require ('colony')
 local enemyColony = colony.createColony({owner = "enemy1"})
 local myColonys = {}
 
-for i = 1, 10 do
+for i = 1, 20 do
     myColonys[i] = colony.createColony({owner = "own"})
     myColonys[i].bacteries:sendArmy(50, myColonys[i], enemyColony)
 end
